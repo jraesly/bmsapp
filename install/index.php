dd81eebce26c74c1355c4ec752b7382e0338220a
John Raesly <john.raesly@gmail.com>
2017-02-16 21:23:08 -0500

    Update

--- install/index.php
+++ install/index.php
@@ -46,36 +46,9 @@
         Please Read Before Continue</h5>
 		  <p><strong>About</strong><br>
 		    Application Name: BmsAPP<br>
-		    By: Razib Miah<br>
-		    <br>
-		    <strong>Getting Support</strong><br>
-		    We are available for any types of support or Customization for this application. You can reach us any of the following methods-<br>
-		    <strong>Email:</strong> sales@bdinfosys.com<br>
-		    Or send us message using our profile page.
-
-		   
-		    <br>
-		    <strong>License Summary</strong><br>
-		   The Regular License grants you, the purchaser, an ongoing, non-exclusive, worldwide license to make use of the digital work (Item) you have selected. <br>
-		   <br>
-           You are licensed to use the Item to create one single End Product for yourself or for one client (a "single application"), and the End Product can be distributed for Free.<br>
-You can create one End Product for a client, and you can transfer that single End Product to your client for any fee. This license is then transferred to your client.<br><br>
-
-<strong>You can't Sell the End Product, except to one client. </strong><br><br>
-
-
-<strong>You can't re-distribute the Item as stock, in a tool or template, or with source files. You can't do this with an Item either on its own or bundled with other items, and even if you modify the Item. You can't re-distribute or make available the Item as-is or with superficial modifications. These things are not allowed even if the re-distribution is for Free.</strong><br><br>
-
-<strong>Although you can modify the Item and therefore delete unwanted components before creating your single End Product, you can't extract and use a single component of an Item on a stand-alone basis.</strong>
-
-<br><br>
-This license can be terminated if you breach it. If that happens, you must stop making copies of or distributing the End Product until you remove the Item from it.<br>
-<br>
-The author of the Item retains ownership of the Item but grants you the license on these terms. This license is between the author of the Item and you. Envato Pty Ltd is not a party to this license or the one giving you the license.<br>
-<br>
-Read The Full License Here-<br>
-<a href="http://codecanyon.net/licenses/regular" target="_blank">http://codecanyon.net/licenses/regular</a> </p>
-		
+		    By: John Raesly<br>
+		    <br>		   
+		    	
 		</div>
 
 </div>
@@ -86,7 +59,7 @@ Read The Full License Here-<br>
 </div>
 <!--  contents area end  -->
 </div>
-<div class="footer">Copyright &copy; 2016 All Rights Reserved<br />
+<div class="footer">Copyright &copy; 2017 All Rights Reserved<br />
 <br />
 </div>
     <script src="../assets/js/bootstrap.min.js"></script>
